a8551cba550fcc0165037b0f97dc1d87c6982672
s5812200333 <s5812200333@gmail.com>
2022-01-18 15:17:22 +0800

    v2-6

---
+++ index2-6.js
@@ -0,0 +1,9 @@
+// 一 .算數運算子: +,-,*,/,%,
+let n1 = 10;
+let n2 = 2;
+let s1 = '1';
+console.timeLog(n1 + parseInt(s1) );
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
