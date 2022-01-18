28a74854c0c3ac711299175c2f5f250a2e0bccfd
s5812200333 <s5812200333@gmail.com>
2022-01-18 09:34:01 +0800

    20200118

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // 註解 ctrl + /
 // alert('Hello world123');
 document.write("<h1>Hello World</h1>");
+/**
+ * hhdhdhd
+ * adjfhaksdf
+ * adfhkasdhfa
+ * adjfhakdf
+ */
---
+++ index2-3.js
@@ -0,0 +1,15 @@
+// x = 5
+// let x;
+// console.log(x);
+// x = 5;
+// console.log(x);
+// x = 'tej';
+// console.log(x);
+
+// let x =5 ;
+// X = 6;
+// console.log(x);
+
+
+const PI = 3.14159;
+//PI = 5;
